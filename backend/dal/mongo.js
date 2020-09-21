49428e2544086a6aea84c3b80878109df95ab608
Hloveday99 <harrisonlovedayawsome@gmail.com>
2020-09-21 15:55:11 -0600

    Added frontend

--- backend/dal/mongo.js
+++ backend/dal/mongo.js
@@ -7,8 +7,7 @@ let client
 function connect(locals) {
 
     console.log("Connecting")
-    const uri = `mongodb+srv://Hloveday99:${process.env.DB_PASSWORD}@cluster0.i2ar2.mongodb.net/<dbname>?retryWrites=true&w=majority`
-
+    
     client = new MongoClient(uri,{useUnifiedTopology: true})
 
     return client.connect()
